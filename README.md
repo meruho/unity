78df0e26c9925f1bb57b10e1b4871d813c0689e0
Meruho <meruho90@gmail.com>
2021-08-05 13:38:23 +0900

    0805

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# Unity
-
+## CODE
 - iTween정리 [iTweenExam.cs](./code/iTweenExam.cs)
 - ARCore정리 [ARCoreCtrl.cs](./code/ARCoreCtrl.cs)
 - 구글광고AdMob [Admob](./code/Admob)
+- 한글조합 [HangulCombination.cs](./code/HangulCombination.cs)
+
+## PATTERN
+- 싱글톤 [Singleton.cs](./code/singleton.cs)
+## MANAGER
+- 사운드매니저 [SoundManager.cs](./code/SoundManager.cs)
+
---
+++ code/HangulCombination.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Unity.Collections.LowLevel.Unsafe;
+
+public class HangulCombination : MonoBehaviour
+{
+    private string row_chosung = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ";
+    private string row_jungsung = "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ";
+    private string row_jongsung = " ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ";
+    // Start is called before the first frame update
+    void Start()
+    {
+        string result = ResultKorean("ㅇ", "ㅝ", "ㄱ"); //읭
+        Debug.Log(result);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private string ResultKorean(string chosung, string jungsung, string jongsung)
+    {
+        int chosungIndex = row_chosung.IndexOf((char)chosung[0]);
+        int jungsungIndex = row_jungsung.IndexOf((char)jungsung[0]);
+        int jongsungIndex = row_jongsung.IndexOf((char)jongsung[0]);
+
+        //공식
+        int result = ((chosungIndex * 588) + ((jungsungIndex * 28) + jongsungIndex)) + 44032;
+
+        char temp = Convert.ToChar(result);
+        return temp.ToString();
+    }
+}
